874b75a7692f98fa9cc2e1ec920a11d9bd2e7128
Nikhil Gurjar <nikhilgurjar2411@gmail.com>
2024-02-07 19:22:07 +0530

    Delete Lyft Backend Software Engineering directory

--- Lyft Backend Software Engineering/serviceable.py
+++
@@ -1,7 +0,0 @@
-from abc import ABC, abstractmethod
-
-# Serviceable is an abstract base class (ABC) with a single abstract method needs_service.
-class Serviceable(ABC):
-    @abstractmethod
-    def needs_service(self):
-        pass
